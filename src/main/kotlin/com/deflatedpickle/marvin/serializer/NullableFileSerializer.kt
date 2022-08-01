a52a9f7a9991721b9d34ae793e0ab1544bb1f4c3
DeflatedPickle <DeflatedPickle@gmail.com>
2022-08-01 21:08:05 +0100

    Added a serializer for nullable files

---
+++ src/main/kotlin/com/deflatedpickle/marvin/serializer/NullableFileSerializer.kt
@@ -0,0 +1,32 @@
+package com.deflatedpickle.marvin.serializer
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.io.File
+
+@ExperimentalSerializationApi
+@Serializer(forClass = File::class)
+object NullableFileSerializer : KSerializer<File?> {
+    override val descriptor = PrimitiveSerialDescriptor(
+        serialName = "Mode",
+        kind = PrimitiveKind.STRING,
+    )
+
+    override fun serialize(encoder: Encoder, value: File?) =
+        encoder.encodeString(value?.absolutePath ?: "null")
+
+    override fun deserialize(decoder: Decoder): File? {
+        decoder.decodeString().let {
+            return if (it == "null") {
+                null
+            } else {
+                File(it)
+            }
+        }
+    }
+}
