d6adfd3e8c7158d0c19b98eba6a4072951a7b174
DeflatedPickle <deflatedpickle@gmail.com>
2023-05-22 19:24:32 +0100

    Added colour lighten and darken extension functions

---
+++ src/main/kotlin/com/deflatedpickle/marvin/extensions/Color.kt
@@ -0,0 +1,22 @@
+package com.deflatedpickle.marvin.extensions
+
+import java.awt.Color
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.roundToInt
+
+fun Color.lighten(fraction: Double, step: Int) =
+    Color(
+        min(255.0, this.red + step * fraction).roundToInt(),
+        min(255.0, this.green + step * fraction).roundToInt(),
+        min(255.0, this.blue + step * fraction).roundToInt(),
+        this.alpha
+    )
+
+fun Color.darken(fraction: Double, step: Int) =
+    Color(
+        max(0.0, this.red - step * fraction).roundToInt(),
+        max(0.0, this.green - step * fraction).roundToInt(),
+        max(0.0, this.blue - step * fraction).roundToInt(),
+        this.alpha
+    )
