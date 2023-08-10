67b038c31121f21c90d29e2270ef96b1218515ad
DeflatedPickle <deflatedpickle@gmail.com>
2023-08-10 23:33:49 +0100

    Added an extension to count digits in an int

--- src/main/kotlin/com/deflatedpickle/marvin/functions/extensions/Int.kt
+++ src/main/kotlin/com/deflatedpickle/marvin/functions/extensions/Int.kt
@@ -2,4 +2,12 @@
 
 package com.deflatedpickle.marvin.functions.extensions
 
+import kotlin.math.abs
+import kotlin.math.log10
+
 operator fun Int.plus(other: Boolean) = this + other.toInt()
+
+fun Int.length() = when(this) {
+    0 -> 1
+    else -> log10(abs(toDouble())).toInt() + 1
+}
